d5692ab536b617d24f71754f1380f0478a9c7e11
Rishi <rishiekandala@gmail.com>
2024-10-22 10:49:46 +0530

    hono setup

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,15 @@
+import { Hono } from 'hono'
+import authors from "./authors"
+import books from "./books"
+import { handle } from 'hono/vercel'
+
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+app.route("/authors", authors)
+app.route("/books", books)
+
+export const GET = handle(app)
+export const POST = handle(app)
