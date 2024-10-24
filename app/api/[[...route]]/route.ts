83d4a9862f68e2678ec9640661a900c7035bc8a0
Rishi <rishiekandala@gmail.com>
2024-10-24 13:26:39 +0530

    Accounts GET, POST handler setup

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,3 +1,4 @@
+import  accounts  from './accounts'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
@@ -6,11 +7,9 @@ export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get("/hello", (c) => {
-    return c.json({
-        message:" HEllo worldd"
-    })
-})
+const routes = app.route("/accounts", accounts);
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
+
+export type AppType = typeof routes;
