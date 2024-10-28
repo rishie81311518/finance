2ae15ef4bafae4f5c7a1a48c2b6da9c423bdd941
Rishi <rishiekandala@gmail.com>
2024-10-28 22:56:37 +0530

    transactions hooks

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -133,6 +133,44 @@ const [data] = await db.insert(transactions).values({
 
 })
 .post(
+  "/bulk-create",
+  clerkMiddleware(),
+  zValidator(
+    "json",
+    z.array(
+      insertTransactionSchema.omit({
+        id: true,
+      }),
+    ),
+  ),
+  async (c) => {
+    const auth = getAuth(c);
+   const values = c.req.valid("json");
+
+   if(!auth?.userId){
+    return c.json({
+      error: "Unauthorized"
+    }, 401)
+   };
+
+   const data = await db
+     .insert(transactions)
+     .values(
+      values.map((value) => ({
+        id: createId(),
+        ...value,
+      }))
+     )
+     .returning();
+
+     return c.json({ data });
+  
+  }
+
+
+)
+
+.post(
   "/bulk-delete",
   clerkMiddleware(),
   zValidator(
