623ff1e43570f8b70fb7485550d15e022a79ce66
Rishi <rishiekandala@gmail.com>
2024-11-03 12:03:00 +0530

    Update transactions components and utilities, add new UI components

--- providers/sheet-provider.tsx
+++ providers/sheet-provider.tsx
@@ -4,6 +4,7 @@ import { NewAccountSheet } from "@/features/accounts/components/new-account-shee
 import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 import { NewCategorySheet } from "@/features/categories/components/new-category-sheet";
 import { EditCategorySheet } from "@/features/categories/components/edit-category-sheet";
+import { NewTransactionSheet } from "@/features/transactions/components/new-transaction-sheet";
 import { useEffect, useState } from "react";
 
 export const SheetProvider = () => {
@@ -25,6 +26,9 @@ export const SheetProvider = () => {
         
         <NewCategorySheet/>
         <EditCategorySheet/>
+         
+         <NewTransactionSheet/>
+
         </>
     )
 }
